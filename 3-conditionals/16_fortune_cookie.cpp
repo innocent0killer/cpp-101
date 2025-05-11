c0a9fc4b080fcb018c0b4aff7cf5432275e42520
innocent0killer <21tiwarivaibhav@gmail.com>
2025-05-11 22:32:30 +0530

    Update 16_fortune_cookie.cpp

--- 3-conditionals/16_fortune_cookie.cpp
+++ 3-conditionals/16_fortune_cookie.cpp
@@ -1,6 +1,28 @@
 // Fortune Cookie 🥠
 // Codédex
+/* they don't have c course so #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+  int k ;
+  srand(time(NULL));    
+  char *l[6] ={ "Don't pursue happiness,  create it.", "All things are difficult before they are easy.", "The early bird gets the worm, but the second mouse gets the cheese.","Someone in your life needs a letter from you.","The fortune you search for is in another cookie.","Help! I'm being held prisoner in a Chinese bakery!"};  
+  k = rand() % 6;
+  printf ("%s \n", l[k]);
+   int i;
+  int g = 50; 
+ for ( i = 0; i < 5; i++)
+ {
+  int m = rand() % g+1 ;
+  printf("%d \t", m);
+ }
+ 
+ 
+ 
 
+  
+  return 0;
+}*/
 #include <iostream>
 #include <cstdlib>
 
